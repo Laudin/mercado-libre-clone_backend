81b18ad55eb46e55db404a2f3f96fbdeb1fd5db0
Laudin <gastonlaudin@gmail.com>
2022-09-03 21:27:42 -0300

    heroku

--- server.ts
+++ server.ts
@@ -40,8 +40,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser('Cookie_Secret'))
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use('/public', express.static('public'))
+//app.use(express.static(path.join(__dirname, 'public')))
 app.use('/static', express.static('static'))
 
 function authorizeUser(req: Request, res: Response, next: CallableFunction) {
